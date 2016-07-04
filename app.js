56684b194a9007c03f2ee2037c7b50b0b7d61bfb
dongne <neal.dong@oocl.com>
2016-07-04 10:54:07 +0800

    remove unnecessary files

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-var rec = require('./rec.js');
-var url = 'http://msict.nbport.com.cn/webapp/jsp/randcode1.jsp';
-var imgName = 'image/test.tif';
-var imageNameProcessed = 'image/test1.tif';
+var fs = require('fs');
+var tesseract = require('node-tesseract');
+var gm = require('gm').subClass({imageMagick:true});
+var imgName = 'image/1.jpg';
+var imageNameProcessed = 'image/2.jpg';
+processImg(imgName,imageNameProcessed)
+		.then(recognizer)
+		.then(text => {
+        console.log(`识别结果:${text}`);
+    })
+    .catch((err)=> {
+        console.error(`识别失败:${err}`);
+    });
 
-var text;
-rec.downloadImg(url,imgName,text, a);
-//console.log('the final result is '+text);
-console.log(a());
-function a () {
-	var test = text;
-	console.log('the final result is '+test);
-	return test;
+
+function processImg(imgName,imageNameProcessed)
+{
+	return new Promise((resolve,reject) => {
+		gm(imgName)
+		.colorspace('gray')
+		.normalize()
+		.threshold('50%')
+		.write(imageNameProcessed,function(err)
+		{
+			if (err)
+				return reject(err);
+			resolve(imageNameProcessed);
+		});
+	});
+}
+
+function recognizer(imageNameProcessed)
+{
+	return new Promise((resolve,reject) => {
+		options = {psm:7};
+		tesseract.process(imageNameProcessed,options,function(err,text)
+		{
+			if (err)
+			{
+				return reject(err);
+			}
+			fs.writeFile('1.txt',text);
+			resolve(text);
+		})
+	})
 }
